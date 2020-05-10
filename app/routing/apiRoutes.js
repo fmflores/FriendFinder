847ff631c96be04270d498a9ece8dab076d07edd
Frank Flores <frankthetank67@gmail.com>
2020-05-09 17:10:18 -0700

    added friends to friends.js file, changed friends.js file to a json file to write to it easier, added bootstrap modal and script code to survey.html file to use jquery

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,30 @@
 const fs = require("fs");
 const path = require("path");
-
+const friendsList = require("../data/friends.json");
 
 function apiRoutes(app) {
     app.get("/api/friends", (req, res) => {
-        // return res.json()
-        //return res.json()
-        res.send("this is the get request working!");
+        //res.send("this is the get request working!");
+        return res.json(friendsList);
     })
 
-   app.post("/api/friends" , (req, res) => {
-       res.send("this is the post route working!");
+   app.post("/api/friends" , ({body}, res) => {
+       console.log('body',body);
+        let bestfriend = null;
+        let diff = Infinity;
+        friendsList.map(friend => {
+            let sum = 0;
+            for(let i = 0 ; i<friend.scores.length; i++){
+                sum += Math.abs(friend.scores[i]-body.scores[i])
+            }
+            if(sum<diff){
+                bestfriend = friend;
+                diff = sum;
+            }
+        });
+        friendsList.push(body);
+        fs.writeFile("./app/data/friends.json", JSON.stringify(friendsList), (err)=>console.log(err || "successfully saved!"))
+       res.send(bestfriend);
     })
 }
 
